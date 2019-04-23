4de7e5e33f567133c0851e746ef292eea20681bc
Eric Pooser <pooser@jlab.org>
2019-04-23 16:55:41 -0400

    more updates to toy det

--- scripts/daq/toy-det/detSig.py
+++ scripts/daq/toy-det/detSig.py
@@ -22,14 +22,19 @@ class dataStream :
         self.randTimeIndex = np.random.randint(int (len(self.timeSamples)*0.25), 
                                                high = int (len(self.timeSamples)*0.75))
     # method to simulate adc sample
-    def simAdcSignals(self, numSamples, randTimeIndex) :
+    # def simAdcSignals(self, numSamples, randTimeIndex) :
+    def simAdcSignals(self) :
         self.baseLine   = np.random.rand(int (self.numSamples))*1.0e-2
-        self.gumbelMean = np.random.random()
-        self.gumbelBeta = np.random.random()
-        self.gumbelPpf  = np.linspace(gumbel_r.ppf(0.001, loc = self.gumbelMean, scale = self.gumbelBeta), 
+        self.hitJudge   = np.random.random()
+        if (self.hitJudge < 0.5) :    # simulate no hit
+            self.adcSamples = self.baseLine
+        elif (self.hitJudge >= 0.5) : # simulate hit
+            self.gumbelMean = np.random.random()
+            self.gumbelBeta = np.random.random()
+            self.gumbelPpf  = np.linspace(gumbel_r.ppf(0.001, loc = self.gumbelMean, scale = self.gumbelBeta), 
                                       gumbel_r.ppf(0.999, loc = self.gumbelMean, scale = self.gumbelBeta), 100)
-        self.gumbelPdf  = gumbel_r.pdf(self.gumbelPpf, loc = self.gumbelMean, scale = self.gumbelBeta)
-        self.adcSamples = np.insert(self.baseLine, self.randTimeIndex, self.gumbelPdf)[:int (self.numSamples)]
+            self.gumbelPdf  = gumbel_r.pdf(self.gumbelPpf, loc = self.gumbelMean, scale = self.gumbelBeta)
+            self.adcSamples = np.insert(self.baseLine, self.randTimeIndex, self.gumbelPdf)[:int (self.numSamples)]
 
     def __iter__(self) :
         return self
@@ -38,10 +43,23 @@ class dataStream :
         if (self.initTime == 0.0) :
             # simulate time and adc samples
             self.simTimeSamples(self.initTime, self.windowWidth)
-            self.simAdcSignals(self.numSamples, self.randTimeIndex)           
+            # self.simAdcSignals(self.numSamples, self.randTimeIndex)
+            self.simAdcSignals()
         elif (self.initTime > 0.0 and self.initTime < self.runTime) :
             # simulate the time and adc signals
             self.simTimeSamples(self.initTime, self.initTime + self.windowWidth)
-            self.simAdcSignals(self.numSamples, self.randTimeIndex)          
+            # self.simAdcSignals(self.numSamples, self.randTimeIndex)
+            self.simAdcSignals()
         elif (self.initTime >= self.runTime) : 
-            raise StopIteration                                           
+            raise StopIteration
+
+sampleRate  = 5.0e+6  # s^-1
+windowWidth = 1.0e-4  # s
+runLength   = 50.0e-4 # s
+numChans    = 3
+ 
+for chan in range(1, numChans) : 
+    dataObj = dataStream(sampleRate, windowWidth, runLength)
+    for event in dataObj : 
+        plt.plot(dataObj.timeSamples, dataObj.adcSamples) 
+    plt.show()
